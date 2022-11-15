b956e467e90b42990b290b5ef8f0d5f46bd8642d
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-15 11:49:37 +0200

    update

--- shell.h
+++ shell.h
@@ -98,13 +98,13 @@ int _strncmp(const char *s1, const char *s2, size_t n);
 
 /* Builtins */
 int (*get_builtin(char *command))(char **args, char **front);
-int shellby_exit(char **args, char **front);
-int shellby_env(char **args, char __attribute__((__unused__)) **front);
-int shellby_setenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_unsetenv(char **args, char __attribute__((__unused__)) **front);
-int shellby_cd(char **args, char __attribute__((__unused__)) **front);
-int shellby_alias(char **args, char __attribute__((__unused__)) **front);
-int shellby_help(char **args, char __attribute__((__unused__)) **front);
+int unishell_exit(char **args, char **front);
+int unishell_env(char **args, char __attribute__((__unused__)) **front);
+int unishell_setenv(char **args, char __attribute__((__unused__)) **front);
+int unishell_unsetenv(char **args, char __attribute__((__unused__)) **front);
+int unishell_cd(char **args, char __attribute__((__unused__)) **front);
+int unishell_alias(char **args, char __attribute__((__unused__)) **front);
+int unishell_help(char **args, char __attribute__((__unused__)) **front);
 
 /* Builtin Helpers */
 char **_copyenv(void);
